c3c06eca1ef4a3419832d8c424f41b9c49f1c6ee
Minh Tran <minh.tran@delta.com>
2025-04-28 13:54:01 -0500

    add tasks and angular

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NewTaskData } from '../task/task.model';
+import { NewTaskData, Task } from '../task/task.model';
+import { TasksService } from '../tasks.service';
+
 
 
 @Component({
@@ -10,22 +12,26 @@ import { NewTaskData } from '../task/task.model';
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
+
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>();
-  @Output() add = new EventEmitter<NewTaskData>();
+  @Input({required: true}) userId!: string;
+  @Output() close = new EventEmitter<void>();
   enteredTitle = '';
   enteredSummary = '';
   enteredDueDate = '';
+
+  private tasksService = inject(TasksService);
   
   onCancelAddTask(){
-    this.cancel.emit();
+    this.close.emit();
   }
 
   onSubmit() {
-    this.add.emit({
+    this.tasksService.addTask({
       title: this.enteredTitle,
       summary: this.enteredSummary,
       dueDate: this.enteredDueDate
-    });
+    }, this.userId);
+    this.close.emit();
   }
 }
--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,6 +1,8 @@
+import { Injectable } from "@angular/core";
 import { NewTaskData } from "./task/task.model";
 
-class TasksService{
+@Injectable({providedIn: 'root'})
+export class TasksService{
     private tasks = [
         {
           id: 't1',
@@ -27,6 +29,14 @@ class TasksService{
         },
     ];
     
+    constructor(){
+      const tasks = localStorage.getItem('tasks');
+
+      if (tasks) {
+        this.tasks = JSON.parse(tasks);
+      }
+    }
+    
     getUserTasks(userId: string){
         return this.tasks.filter(task => task.userId === userId);
     }
@@ -39,9 +49,15 @@ class TasksService{
             summary: taskData.summary,
             dueDate: taskData.dueDate
           });
+          this.saveTasks();
     }
 
     removeTask(taskId: string){
         this.tasks = this.tasks.filter(task => task.id !== taskId);
+        this.saveTasks();
+    }
+
+    private saveTasks() {
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
     }
 }
